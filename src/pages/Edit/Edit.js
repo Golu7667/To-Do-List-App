5152d21bc0548724357567689d6ca5c84fa93870
unknown <goluchoudhary7808@gmail.com>
2023-03-22 23:44:51 +0530

    nextjs

---
+++ src/pages/Edit/Edit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import  "./edit.css"
+function Edit() {
+  return (
+    <div>Edit</div>
+  )
+}
+
+export default Edit
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./register.css"
+function Register() {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
